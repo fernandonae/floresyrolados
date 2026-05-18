7509bfc9b50a02051a2557590498f89dff3234a1
Fernando Martin Rivas Flores <21680156Q@cuutla.tecnm.mx>
2026-05-17 20:57:07 -0600

    Cambiando url de local a produccion

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+// Si existe la variable en Vercel, la usa. Si no, usa localhost para cuando tú programes.
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
 const instance = axios.create({
-  
-  baseURL: "http://localhost:5000/api", 
+  baseURL: API_URL, 
   withCredentials: true,
 });
-
 
 export default instance;
